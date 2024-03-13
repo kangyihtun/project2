35872b1d3e7c15e83a99426f3a074b3edd055c0f
Kan Gyi Htun <kangyihtun2006@gmail.com>
2024-03-13 10:50:30 +0630

    email box

---
+++ app.js
@@ -0,0 +1,14 @@
+// Get UI
+const getemail = document.getElementById('emails');
+const gettextarea = document.querySelector('textarea');
+const getbtn = document.querySelector('.btn');
+const getemcontainer = document.querySelector('.email-container');
+
+getemail.focus();
+
+getemail.addEventListener('keyup',function(e){
+    // console.log(e.target.value);
+    // console.log(this.value);
+});
+
+// 19EM 
